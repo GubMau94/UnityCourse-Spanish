d235e2083b40b35cf23b2b37ed722ed4d16577fc
GubMau94 <guberman.mn@gmail.com>
2021-06-15 23:47:48 +0200

    Main Menu added

---
+++ Projects/BallChaser/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _mainMenu, _resume;
+
+    private void Start()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        _mainMenu.SetActive(true);
+        _resume.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+        Screen.orientation = ScreenOrientation.Portrait;
+        _mainMenu.SetActive(false);
+        _resume.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        _mainMenu.SetActive(false);
+        _resume.SetActive(false);
+    }
+}
